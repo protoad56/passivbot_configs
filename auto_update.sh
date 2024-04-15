ce8ed32d976710b8bdc3ad0a52fe8cc5f5918418
protoad56 <adatt56@gmail.com>
2024-04-15 23:01:05 +0100

    auto update

--- auto_update.sh
+++ auto_update.sh
@@ -7,12 +7,66 @@ TG_CHAT_ID="6757461113"
 
 ROOT_PATH="/root"
 CFG_DIR="${ROOT_PATH}/passivbot_configs"
-PASSIVBOT_DIR="${ROOT_PATH}/passivbot/"
+PASSIVBOT_DIR="${ROOT_PATH}/passivbot"
 
 GIT_URL="https://github.com/protoad56/passivbot_configs.git"
 
+# Define the source folder and the destination folder
+source_folder="${ROOT_PATH}/passivbot_configs/optimized"
+destination_folder="${ROOT_PATH}/passivbot/configs/live/forager/live"  
+
+# Define the path to the lock file
+lock_file="${PASSIVBOT_DIR}/passivbot/configs/forager/forager_lock.json"
 
 cd $CFG_DIR
+
+# Function to check if a file is locked
+is_locked() {
+    jq -r --arg filename "$1" '.files[] | select(.filename == $filename)' "$lock_file" | grep -q .
+}
+
+
+# Function to send message to Telegram
+send_telegram_message() {
+    local message="$1"
+        curl -s --data "text=${message}" \
+                --data "reply_markup=${keyboard}" \
+                --data "chat_id=$TG_CHAT_ID" \
+                --data "parse_mode=markdown" \
+                "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+}
+
+# Function to copy files based on newer timestamp
+copy_if_newer() {
+    src_file="$1"
+    dest_file="$2"
+    if [ -f "$dest_file" ]; then
+        # Compare file modification times
+        if [ "$src_file" -nt "$dest_file" ]; then
+            if ! is_locked "$(basename "$src_file")"; then
+                cp "$src_file" "$dest_file"
+                message="Copied updated file: $(basename "$src_file")"
+                echo "$message"
+                send_telegram_message "$message"
+            else
+                message="Skipping locked file: $(basename "$src_file")"
+                echo "$message"
+                send_telegram_message "$message"
+            fi
+        fi
+    else
+        if ! is_locked "$(basename "$src_file")"; then
+            cp "$src_file" "$dest_file"
+            message="Copied new file: $(basename "$src_file")"
+            echo "$message"
+            send_telegram_message "$message"
+        else
+            message="Skipping locked file: $(basename "$src_file")"
+            echo "$message"
+            send_telegram_message "$message"
+        fi
+    fi
+}
 
 
 # Get current commit
@@ -40,4 +94,16 @@ if [ "$latest_commit" != "$current_commit" ]; then
                 --data "chat_id=$TG_CHAT_ID" \
                 --data "parse_mode=markdown" \
                 "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+
+        # Iterate over files in the source folder
+        for file in "$source_folder"/*; do
+            if [ -f "$file" ]; then
+                filename=$(basename "$file")
+                dest_file_path="$destination_folder/$filename"
+                copy_if_newer "$file" "$dest_file_path"
+            fi
+        done
+
+        copy_if_newer att_backtest_result.csv "$PASSIVBOT_DIR/att_backtest_result.csv"
+
 fi
