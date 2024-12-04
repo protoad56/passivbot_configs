f00d6061c05c2c0bfa894e5aef87216d1a6153a5
protoad56 <adatt56@gmail.com>
2024-12-04 08:43:05 +0700

    add new config

--- start_passivbot.sh
+++ start_passivbot.sh
@@ -9,6 +9,8 @@ create_tmux_session() {
 
     if [ $? != 0 ]; then
         tmux new-session -d -s "$session_name" "$command_to_run"
+        echo "Starting $session_name"
+        send_telegram_message "6757461113" "Starting $session_name" "6951567916:AAETarfcLbySoCkI6zYPSZj09Pu3kALmlUw"
     else
         echo "Session $session_name already exists."
     fi
@@ -21,12 +23,16 @@ create_tmux_session "hyper_main" "cd /root/passivbot_multi/ && source myenv/bin/
 sleep 60
 create_tmux_session "doge" "cd /root/passivbot_multi/ && source myenv/bin/activate && python3 passivbot.py binance_02 DOGEUSDT ../passivbot/configs/live/clock_0days.json -lw 2.5 -lm n -sm gs"
 sleep 60
-create_tmux_session "doge_max" "cd /root/passivbot_multi/ && source myenv/bin/activate && python3 passivbot.py binance_03 INJUSDT ../passivbot_configs/clock/INJUSDT.json -lw 2.0 -sw 1.0 -lm n -sm gs"
+# create_tmux_session "doge_max" "cd /root/passivbot_multi/ && source myenv/bin/activate && python3 passivbot.py binance_03 INJUSDT ../passivbot_configs/clock/INJUSDT.json -lw 2.0 -sw 1.0 -lm n -sm gs"
+# sleep 60
+# create_tmux_session "multi" "cd /root/passivbot_multi/ && source myenv/bin/activate && python3 forager_improved_multi_prototype.py configs/forager/config_multi_live.hjson"
+create_tmux_session "hawk" "cd /root/hawkbot/ && source myenv/bin/activate && python3 trade.py -a binance_forager -c ../passivbot_configs/hawk/dynamic-lin.json"
 sleep 60
-create_tmux_session "multi" "cd /root/passivbot_multi/ && source myenv/bin/activate && python3 forager_improved_multi_prototype.py configs/forager/config_multi_live.hjson"
-
+create_tmux_session "hyper_attcrypto" "cd /root/passivbot_v7 && source venv/bin/activate && cd passivbot-rust && maturin develop --release && cd .. && python3 src/main.py ../passivbot_configs/v7/template-att-multi.json -u hyperliquid_01 -lnp 10"
+sleep 60
+# create_tmux_session "hyper_v7" "cd /root/passivbot_v7 && source venv/bin/activate && cd passivbot-rust && maturin develop --release && cd .. && python3 src/main.py ../passivbot_configs/v7/2024-09-19T20_22_20_8_coins_7316a4a7.json"
+# sleep 60
 cd /root/exchanges_dashboard2/
-
 docker-compose up -d
 
 
